dc2a71094812ff5fde4e692b022ac3cb77b15226
Dawid Grad <dawidgrad.work@gmail.com>
2023-05-01 19:08:37 +0100

    Tab functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Note from './Note';
+import { AppBar, Tabs, Tab, Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import React, { useState } from 'react';
+
+const ActiveTabContent = styled(Box)`
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 5px;
+  padding: 16px;
+  margin-top: 16px;
+`;
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [tabValue, setTabValue] = useState(0);
 
-  const addNote = () => {
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
+  const handleAddNote = () => {
     setNotes([
       ...notes,
       {
@@ -18,16 +32,38 @@ function App() {
 
   return (
     <div className="App">
-      <div className='buttons-container'>
-        <button className='add-note-button' onClick={addNote}>
-          Add note
-        </button>
-      </div>
-      <div className='notes-container'>
-        {notes.map(note => (
-          <Note key={note.id} title={note.title} content={note.content} />
-        ))}
-      </div>
+      <AppBar position="static" color='default'>
+        <Tabs value={tabValue} onChange={handleTabChange} indicatorColor="primary" textColor="primary" variant="fullWidth">
+          <Tab label="Notes" />
+          <Tab label="Topics" />
+          <Tab label="Settings" />
+        </Tabs>
+      </AppBar>
+      {tabValue === 0 && (
+        <ActiveTabContent>
+          <div className='buttons-container'>
+            <button className='add-note-button' onClick={handleAddNote}>
+              Add note
+            </button>
+          </div>
+          <div className='notes-container'>
+            {notes.map(note => (
+              <Note key={note.id} title={note.title} content={note.content} />
+            ))}
+          </div>
+        </ActiveTabContent>
+      )}
+      {tabValue === 1 && (
+        <ActiveTabContent>
+          <h1>Topics</h1>
+        </ActiveTabContent>
+      )}
+      {tabValue === 2 && (
+        <ActiveTabContent>
+          <h1>Settings</h1>
+        </ActiveTabContent>
+      )}
+      
     </div>
   )
 }
